46ab8058e0d8679132d2a06e26e737d06871b8ef
Austin David Hooley <austindavidhooley@austins-mbp.dhcp.nd.edu>
2019-12-09 10:54:23 -0500

    Moving Sprites

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,146 +1,96 @@
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
+//#include <chrono>
+//#include <cstdint>
+#include <iostream>
+
 #include <SFML/Window.hpp>
-#include <SFML/OpenGL.hpp>
+#include <SFML/Graphics.hpp>
+//#include <SFML/image.hpp>
+//#include <Sprite.hpp>
+using namespace std;
+
+//uint64_t tget(){
+//    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()).count();
+//}
 
 
-////////////////////////////////////////////////////////////
-/// Entry point of application
-///
-/// \return Application exit code
-///
-////////////////////////////////////////////////////////////
 int main()
 {
-    // Request a 24-bits depth buffer when creating the window
-    sf::ContextSettings contextSettings;
-    contextSettings.depthBits = 24;
+    sf::RenderWindow window(sf::VideoMode(1600, 1200), "My window"); // Create Window
+    window.setFramerateLimit(60);     // Cap framerate
 
-    // Create the main window
-    sf::Window window(sf::VideoMode(640, 480), "SFML window with OpenGL", sf::Style::Default, contextSettings);
+    // Create texture names
+    sf::Texture texture1;
+    sf::Texture texture2;
+    sf::Texture texture3;
+    sf::Sprite sprite1;
 
-    // Make it the active window for OpenGL calls
-    window.setActive();
+    // Create sprite names
+    sf::Sprite sprite2;
+    sf::Sprite sprite3;
+    sf::Sprite sprite4;
 
-    // Set the color and depth clear values
-    glClearDepth(1.f);
-    glClearColor(0.f, 0.f, 0.f, 1.f);
+    // Load textures
+    texture1.loadFromFile("/Users/austindavidhooley/Desktop/CProgram/LeprechaunDo/1Lep-1.jpg");
+    texture2.loadFromFile("/Users/austindavidhooley/Desktop/CProgram/LeprechaunDo/1Lep-2.jpg");
+    texture3.loadFromFile("/Users/austindavidhooley/Desktop/CProgram/LeprechaunDo/1Lep-3.jpg");
+    //if (!texture.loadFromFile("1Lep-2.jpg")){
+      //return 1;
+    //}
+    sprite1.setTexture(texture1);
+    sprite2.setTexture(texture2);
+    sprite3.setTexture(texture3);
+    sprite4.setTexture(texture2);
+    //sprite2.setPosition(400,400);
+    //sprite3.setPosition(800,800);
 
-    // Enable Z-buffer read and write
-    glEnable(GL_DEPTH_TEST);
-    glDepthMask(GL_TRUE);
+    // Sprite Scaling
+    sprite1.scale(2,2);
+    sprite2.scale(2,2);
+    sprite3.scale(2,2);
+    sprite4.scale(0.5,0.5);
+    sprite4.setPosition(10,800);
 
-    // Disable lighting and texturing
-    glDisable(GL_LIGHTING);
-    glDisable(GL_TEXTURE_2D);
+    // Music code
+    //sf::Music music;
+    //music.openFromFile();
 
-    // Configure the viewport (the same size as the window)
-    glViewport(0, 0, window.getSize().x, window.getSize().y);
-
-    // Setup a perspective projection
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    GLfloat ratio = static_cast<float>(window.getSize().x) / window.getSize().y;
-    glFrustum(-ratio, ratio, -1.f, 1.f, 1.f, 500.f);
-
-    // Define a 3D cube (6 faces made of 2 triangles composed by 3 vertices)
-    GLfloat cube[] =
-    {
-        // positions    // colors (r, g, b, a)
-        -50, -50, -50,  0, 0, 1, 1,
-        -50,  50, -50,  0, 0, 1, 1,
-        -50, -50,  50,  0, 0, 1, 1,
-        -50, -50,  50,  0, 0, 1, 1,
-        -50,  50, -50,  0, 0, 1, 1,
-        -50,  50,  50,  0, 0, 1, 1,
-
-         50, -50, -50,  0, 1, 0, 1,
-         50,  50, -50,  0, 1, 0, 1,
-         50, -50,  50,  0, 1, 0, 1,
-         50, -50,  50,  0, 1, 0, 1,
-         50,  50, -50,  0, 1, 0, 1,
-         50,  50,  50,  0, 1, 0, 1,
-
-        -50, -50, -50,  1, 0, 0, 1,
-         50, -50, -50,  1, 0, 0, 1,
-        -50, -50,  50,  1, 0, 0, 1,
-        -50, -50,  50,  1, 0, 0, 1,
-         50, -50, -50,  1, 0, 0, 1,
-         50, -50,  50,  1, 0, 0, 1,
-
-        -50,  50, -50,  0, 1, 1, 1,
-         50,  50, -50,  0, 1, 1, 1,
-        -50,  50,  50,  0, 1, 1, 1,
-        -50,  50,  50,  0, 1, 1, 1,
-         50,  50, -50,  0, 1, 1, 1,
-         50,  50,  50,  0, 1, 1, 1,
-
-        -50, -50, -50,  1, 0, 1, 1,
-         50, -50, -50,  1, 0, 1, 1,
-        -50,  50, -50,  1, 0, 1, 1,
-        -50,  50, -50,  1, 0, 1, 1,
-         50, -50, -50,  1, 0, 1, 1,
-         50,  50, -50,  1, 0, 1, 1,
-
-        -50, -50,  50,  1, 1, 0, 1,
-         50, -50,  50,  1, 1, 0, 1,
-        -50,  50,  50,  1, 1, 0, 1,
-        -50,  50,  50,  1, 1, 0, 1,
-         50, -50,  50,  1, 1, 0, 1,
-         50,  50,  50,  1, 1, 0, 1,
-    };
-
-    // Enable position and color vertex components
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glVertexPointer(3, GL_FLOAT, 7 * sizeof(GLfloat), cube);
-    glColorPointer(4, GL_FLOAT, 7 * sizeof(GLfloat), cube + 3);
-
-    // Disable normal and texture coordinates vertex components
-    glDisableClientState(GL_NORMAL_ARRAY);
-    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-
-    // Create a clock for measuring the time elapsed
+    // Start clock
     sf::Clock clock;
 
-    // Start the game loop
+    // run the program as long as the window is open
     while (window.isOpen())
     {
-        // Process events
+        sf::Time elapsed1 = clock.getElapsedTime();
+        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // Close window: exit
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
-
-            // Escape key: exit
-            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
-                window.close();
-
-            // Resize event: adjust the viewport
-            if (event.type == sf::Event::Resized)
-                glViewport(0, 0, event.size.width, event.size.height);
         }
+        //music.play();
 
-        // Clear the color and depth buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Apply some transformations to rotate the cube
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glTranslatef(0.f, 0.f, -200.f);
-        glRotatef(clock.getElapsedTime().asSeconds() * 50, 1.f, 0.f, 0.f);
-        glRotatef(clock.getElapsedTime().asSeconds() * 30, 0.f, 1.f, 0.f);
-        glRotatef(clock.getElapsedTime().asSeconds() * 90, 0.f, 0.f, 1.f);
-
-        // Draw the cube
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        // Finally, display the rendered frame on screen
+        window.clear(sf::Color::Black);
+        //window.draw(sprite2);
+        //window.draw(sprite1);
+        //window.draw(sprite3);
+        window.draw(sprite4);
+        sprite4.move(6,0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+          //sprite2.move(-1,0);
+          window.draw(sprite1);
+          //cout << elapsed1.asSeconds() << endl;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+          //sprite2.move(1,0);
+          window.draw(sprite3);
+        }
+        else window.draw(sprite2);
+        //sprite1.move(1,1);
         window.display();
+
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
